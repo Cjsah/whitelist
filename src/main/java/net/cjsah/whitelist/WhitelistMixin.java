2eb827a7f3ee82dc7f75b6430079c41fa772b770
Cjsah <2684117397@qq.com>
2021-02-10 00:22:23 +0800

    all

---
+++ src/main/java/net/cjsah/whitelist/WhitelistMixin.java
@@ -0,0 +1,22 @@
+package net.cjsah.whitelist;
+
+import com.mojang.authlib.GameProfile;
+import net.minecraft.server.Whitelist;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+
+@Mixin(Whitelist.class)
+public abstract class WhitelistMixin {
+
+	/**
+	 * 白名单匹配名字而不再匹配uuid
+	 * @author Cjsah
+	 * @return name
+	 */
+	@Overwrite
+	public String toString(GameProfile gameProfile) {
+		return gameProfile.getName();
+	}
+
+
+}
